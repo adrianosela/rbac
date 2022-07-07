ea3bcbd8f788d230845286c869862b1f869ab05a
Adriano <adriano.selaviles@gmail.com>
2022-07-06 22:50:59 -0400

    add create/read for role + permission primitives

--- api/service/permission.go
+++ api/service/permission.go
@@ -1,6 +1,13 @@
 package service
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/adrianosela/rbac/api/model"
+	"github.com/gorilla/mux"
+)
 
 func (s *service) setPermissionEndpoints() {
 	s.router.Methods(http.MethodPost).Path("/permission").HandlerFunc(s.createPermissionHandler)
@@ -10,11 +17,55 @@ func (s *service) setPermissionEndpoints() {
 }
 
 func (s *service) createPermissionHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// TODO: get authenticated user from ctx (for ownership)
+	var permission *model.Permission
+	if err := unmarshalRequestBody(r, &permission); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("request body is not a permission"))
+		return
+	}
+	// TODO: add authenticated user to owners if not present
+	// TODO: validate role has mandatory fields populated
+	if err := s.store.CreatePermission(permission); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to create new permission in storage"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Permission \"%s\" created successfuly!", permission.Name)))
+	return
 }
 
 func (s *service) readPermissionHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	name := mux.Vars(r)["name"]
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no permission name in request URL"))
+		return
+	}
+
+	permission, err := s.store.ReadPermission(name)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to read permission from storage"))
+		return
+	}
+	if permission == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Permission \"%s\" does not exist!", name)))
+		return
+	}
+
+	permissionBytes, err := json.Marshal(&permission)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to encode response"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(permissionBytes)
+	return
 }
 
 func (s *service) updatePermissionHandler(w http.ResponseWriter, r *http.Request) {
--- api/service/role.go
+++ api/service/role.go
@@ -1,6 +1,13 @@
 package service
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/adrianosela/rbac/api/model"
+	"github.com/gorilla/mux"
+)
 
 func (s *service) setRoleEndpoints() {
 	s.router.Methods(http.MethodPost).Path("/role").HandlerFunc(s.createRoleHandler)
@@ -10,11 +17,55 @@ func (s *service) setRoleEndpoints() {
 }
 
 func (s *service) createRoleHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	// TODO: get authenticated user from ctx (for ownership)
+	var role *model.Role
+	if err := unmarshalRequestBody(r, &role); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("request body is not a role"))
+		return
+	}
+	// TODO: add authenticated user to owners if not present
+	// TODO: validate role has mandatory fields populated
+	if err := s.store.CreateRole(role); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to create new role in storage"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Role \"%s\" created successfuly!", role.Name)))
+	return
 }
 
 func (s *service) readRoleHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	name := mux.Vars(r)["name"]
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("no role name in request URL"))
+		return
+	}
+
+	role, err := s.store.ReadRole(name)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to read role from storage"))
+		return
+	}
+	if role == nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Role \"%s\" does not exist!", name)))
+		return
+	}
+
+	roleBytes, err := json.Marshal(&role)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to encode response"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(roleBytes)
+	return
 }
 
 func (s *service) updateRoleHandler(w http.ResponseWriter, r *http.Request) {
